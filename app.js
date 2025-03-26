a3a005135d93f38594981f36269e4fc3e05c139b
julien-muke <julien.muke@gmail.com>
2025-03-26 12:15:01 +0200

    🚀 nodejs sample

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Updated CI/CD Pipeline!')
 })
 
 app.listen(port, () => {
